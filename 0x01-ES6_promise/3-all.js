a7353df0828c8bae346dcea208fa579c0f983cbd
leulkashay <leulkashay2012@gmail.com>
2023-06-28 19:16:29 +0000

    update

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+ 
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user])
+     .then((values) => {
+      const profile = values[0].body;
+      const first = values[1].firstName;
+      const last = values[1].lastName;
+
+      console.log(
+        `${profile} ${first} ${last}`,
+      );
+    })
+    .catch(() => console.log('Signup system offline'));
+}
