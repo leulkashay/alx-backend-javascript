adc6cba8c3ad8d026de58395e667a9e16276ce4d
leulkashay <leulkashay2012@gmail.com>
2023-08-23 01:23:53 +0000

    update

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -5,9 +5,9 @@ const port = 1245;
 const hostname = '127.0.0.1';
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 module.exports = app.listen(port, hostname, () => {
-    console.log(`Example app running at http://${hostname}:${port}/`);
+  console.log(`Example app running at http://${hostname}:${port}/`);
 });
